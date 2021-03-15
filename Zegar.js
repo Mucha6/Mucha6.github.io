60b5969b13aa8e9509bc27ee31faf96090953a58
efindus <48939809+efindus@users.noreply.github.com>
2021-03-15 23:16:15 +0100

    Update Zegar.js

--- Zegar.js
+++ Zegar.js
@@ -1,15 +1,13 @@
-function UstawCzas()
+const UstawCzas = () =>
 {
-	var data = new Date();
-	var godzina = data.getHours();
-	if (godzina < 10) godzina = "0" + godzina;
-	var minuta = data.getMinutes();
-	if (minuta < 10) minuta = "0" + minuta;
-	var sekunda = data.getSeconds();
-	if (sekunda < 10) sekunda = "0" + sekunda;
-	document.getElementById("zegar").innerHTML = godzina + ":" + minuta + ":" + sekunda;
-	var dzien = data.getDate();
-	//UstawLink(minuta);
+	const data = new Date();
+	let godzina = data.getHours();
+	if (godzina < 10) godzina = '0' + godzina;
+	let minuta = data.getMinutes();
+	if (minuta < 10) minuta = '0' + minuta;
+	let sekunda = data.getSeconds();
+	if (sekunda < 10) sekunda = '0' + sekunda;
+	document.getElementById("zegar").innerHTML = godzina + ':' + minuta + ':' + sekunda;
 	UstawSuchara(minuta);
-	setTimeout("UstawCzas()", 1000);
+	setTimeout(UstawCzas, 1000);
 }
